18c8788c64837de666a87d0ec730f0072af279a4
Joshua <joshuaugwuede@gmail.com>
2024-02-01 18:23:11 +0100

    wow

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -19,45 +19,48 @@ const HeroSection = () => {
   };
 
   return (
-    <>
-      <Carousel activeIndex={index} onSelect={handleSelect}>
-        <Carousel.Item>
-          <img src={HeroImgFirst} alt="First slide" style={style} />
-          <Carousel.Caption>
-            <h3>Digital Excellence, Realized</h3>
-            <br />
+    <Carousel
+      activeIndex={index}
+      onSelect={handleSelect}
+      style={{ heigth: "100vh" }}
+    >
+      <Carousel.Item>
+        <img src={HeroImgFirst} alt="First slide" style={style} />
+        <Carousel.Caption>
+          <h3>Digital Excellence, Realized</h3>
+          <br />
 
-            <h6>Where Web Development Meets Stunning Design</h6>
+          <p style={{ fontSize: "14px" }}>
+            <span className="fw-bolder">
+              Where Web Development Meets Stunning Design:{" "}
+            </span>
+            Craft an unforgettable online presence with seamless web development
+            and vibrant graphic design. Elevate your brand today.
+          </p>
+          <br />
 
-            <p style={{ fontSize: "14px" }}>
-              Craft an unforgettable online presence with seamless web
-              development and vibrant graphic design. Elevate your brand today.
-            </p>
-            <br />
-
-            <Button>Explore Our Services</Button>
-            <br />
-            <br />
-          </Carousel.Caption>
-        </Carousel.Item>
-        <Carousel.Item>
-          <img src={HeroImgSecond} alt="Second slide" style={style} />
-          <Carousel.Caption>
-            <h3>Second slide label</h3>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-          </Carousel.Caption>
-        </Carousel.Item>
-        <Carousel.Item>
-          <img src={HeroImgThird} alt="Third slide" style={style} />
-          <Carousel.Caption>
-            <h3>Third slide label</h3>
-            <p>
-              Praesent commodo cursus magna, vel scelerisque nisl consectetur.
-            </p>
-          </Carousel.Caption>
-        </Carousel.Item>
-      </Carousel>
-    </>
+          <Button>Explore Our Services</Button>
+          <br />
+          <br />
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item>
+        <img src={HeroImgSecond} alt="Second slide" style={style} />
+        <Carousel.Caption>
+          <h3>Second slide label</h3>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item>
+        <img src={HeroImgThird} alt="Third slide" style={style} />
+        <Carousel.Caption>
+          <h3>Third slide label</h3>
+          <p>
+            Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+          </p>
+        </Carousel.Caption>
+      </Carousel.Item>
+    </Carousel>
   );
 };
 
