7a122f04855713cce6172a75db07cfdb743f2ded
Joshua <joshuaugwuede@gmail.com>
2024-02-03 16:33:07 +0100

    navbar

--- src/components/NavbarComp.jsx
+++ src/components/NavbarComp.jsx
@@ -5,9 +5,11 @@ import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 import NavDropdown from "react-bootstrap/NavDropdown";
 import Offcanvas from "react-bootstrap/Offcanvas";
+import "../custom.css";
 
 function NavbarComp() {
   let expand = "md";
+
   return (
     <>
       <Navbar
@@ -15,34 +17,56 @@ function NavbarComp() {
         fixed="top"
         expand={expand}
         // variant="secondary"
-        // className="bg-body-tertiary mb-0"
+        // className="text-white"
         // bg="secondary"
-        className="bg-body-tertiary mb-3 shadow-sm"
+        // className="bg-body-tertiary mb-3 shadow-sm"
+
         style={{
           boxShadow: "0px 4px 6px rgba(54, 54, 54, 0.1)",
-          // background: "#101105",
+          background: "#101105",
           // color: "white",
         }}
       >
         <Container fluid="lg">
-          <Navbar.Brand href="#">Logo</Navbar.Brand>
-          <Navbar.Toggle aria-controls={`offcanvasNavbar-expand-${expand}`} />
+          <Navbar.Brand href="#" className="text-white">
+            Logo
+          </Navbar.Brand>
+          <Navbar.Toggle
+            className="p-1"
+            style={{ borderColor: "none", background: "white", color: "none" }}
+            aria-controls={`offcanvasNavbar-expand-${expand}`}
+          />
           <Navbar.Offcanvas
             id={`offcanvasNavbar-expand-${expand}`}
             aria-labelledby={`offcanvasNavbarLabel-expand-${expand}`}
             placement="end"
+            // className="bg-primary"
+            style={{
+              background: "#101105d1",
+            }}
           >
             <Offcanvas.Header closeButton>
-              <Offcanvas.Title id={`offcanvasNavbarLabel-expand-${expand}`}>
+              <Offcanvas.Title
+                id={`offcanvasNavbarLabel-expand-${expand}`}
+                className="text-white"
+              >
                 Offcanvas
               </Offcanvas.Title>
             </Offcanvas.Header>
             <Offcanvas.Body>
-              <Nav className="justify-content-end flex-grow-1 pe-3">
-                <Nav.Link href="/">Home</Nav.Link>
-                <Nav.Link href="/services">Our Services</Nav.Link>
-                <Nav.Link href="/about">About Us</Nav.Link>
-                <Nav.Link href="/contact">Contact Us</Nav.Link>
+              <Nav className="justify-content-end flex-grow-1 pe-3 ">
+                <Nav.Link href="/" className="text-white">
+                  Home
+                </Nav.Link>
+                <Nav.Link href="/services" className="text-white">
+                  Our Services
+                </Nav.Link>
+                <Nav.Link href="/about" className="text-white">
+                  About Us
+                </Nav.Link>
+                <Nav.Link href="/contact" className="text-white">
+                  Contact Us
+                </Nav.Link>
               </Nav>
             </Offcanvas.Body>
           </Navbar.Offcanvas>
